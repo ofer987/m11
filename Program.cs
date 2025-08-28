447c5994408fd9f445d2d77570819d84c8763ec0
Dan Jakob Ofer <dan@ofer.to>
2025-08-28 08:49:13 -0400

    refactor(utils): add Utils static class

--- Program.cs
+++ Program.cs
@@ -1,21 +1,19 @@
 ﻿using System.Net;
-using System.Text;
-using System.Net.Http.Json;
 using System.Text.Json;
 
 namespace M11;
 
 public class Program
 {
-    public static string BaseQueryParameter = "?auth=";
-    public static string BaseAddress = "https://api.ensoconnect.com/prod/keycard";
-    public static string FilePath = "results.cs.txt";
+    private static string BaseQueryParameter = "?auth=";
+    private static string BaseAddress = "https://api.ensoconnect.com/prod/keycard";
+    private static string FilePath = "results.cs.txt";
 
-    public static long Size = 2_821_109_907_456;
+    private static long Size = 2_821_109_907_456;
 
     public static int Main(string[] args)
     {
-        CreateFile(FilePath);
+        Utils.CreateFile(FilePath);
 
         Parallel.For(0, Size,
             (j) =>
@@ -23,7 +21,7 @@ public class Program
                 using var client = new HttpClient();
                 client.BaseAddress = new Uri(BaseAddress);
 
-                var queryValue = ConvertToBase36(j);
+                var queryValue = Utils.ConvertToBase36(j);
 
                 var queryParameter = GetQueryParameter(queryValue);
                 var url = $"{BaseAddress}{queryParameter}";
@@ -56,64 +54,5 @@ public class Program
     private static string GetQueryParameter(string input)
     {
         return $"{BaseQueryParameter}{input}";
-    }
-
-    private static void CreateFile(string path)
-    {
-        if (File.Exists(path))
-        {
-            return;
-        }
-
-        File.Open(path, FileMode.Create);
-    }
-
-    private static string ConvertToBase36(long input)
-    {
-        var i = 7;
-        var sb = new StringBuilder();
-        var remaining = input;
-        var remainder = 0L;
-
-        while (i >= 0 && remaining > 0)
-        {
-            remainder = remaining % 36;
-            remaining = remaining / 36;
-
-            if (remainder >= 10)
-            {
-                char ch = ConvertNumberToLowerCaseCharacter(remainder);
-                sb.Append(ch);
-            }
-            else
-            {
-                sb.Append(remainder);
-            }
-
-            i -= 1;
-        }
-
-        var length = sb.Length;
-
-        for (i = length; i < 8; i += 1)
-        {
-            sb.Append(0);
-        }
-
-        var result = sb.ToString();
-        var charArray = result.ToCharArray();
-        Array.Reverse(charArray);
-
-        var reversedResult = new string(charArray);
-
-        return reversedResult;
-    }
-
-    private static char ConvertNumberToLowerCaseCharacter(long input)
-    {
-        // a is 97
-        // but input starts @ 10
-        // So 10 will be 'a' or 97
-        return (char)(input + 87);
     }
 }
